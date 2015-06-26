fd807fdc7416f64df24c14f65a8ba82b37509e89
Hernan Zalazar <hernanzalazar@gmail.com>
2015-06-26 18:33:12 -0300

    Project structure

---
+++ Auth0/Auth0.h
@@ -0,0 +1,19 @@
+//
+//  Auth0.h
+//  Auth0
+//
+//  Created by Hernan Zalazar on 6/24/15.
+//  Copyright (c) 2015 Auth0. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Auth0.
+FOUNDATION_EXPORT double Auth0VersionNumber;
+
+//! Project version string for Auth0.
+FOUNDATION_EXPORT const unsigned char Auth0VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Auth0/PublicHeader.h>
+
+
