0d2ad8f7096b1d8674be3050bacf20a4a2062c9a
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-04-24 21:43:47 -0300

    Update dependencies and fix compile issues

--- Carthage/Build/iOS/Alamofire.framework/Headers/Alamofire.h
+++ Carthage/Build/iOS/Alamofire.framework/Headers/Alamofire.h
@@ -1,6 +1,6 @@
 // Alamofire.h
 //
-// Copyright (c) 2014–2015 Alamofire Software Foundation (http://alamofire.org/)
+// Copyright (c) 2014–2016 Alamofire Software Foundation (http://alamofire.org/)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 FOUNDATION_EXPORT double AlamofireVersionNumber;
 FOUNDATION_EXPORT const unsigned char AlamofireVersionString[];
