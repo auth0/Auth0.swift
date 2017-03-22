55370b9138294b8636af0297642030344ead13ce
Martin Walsh <martin.walsh@gmail.com>
2017-03-22 11:16:23 +0000

    Added protocol method to check native auth availability for provider

--- Auth0/AuthProvider.swift
+++ Auth0/AuthProvider.swift
@@ -35,8 +35,23 @@ import UIKit
          ...
          return transaction
      }
+ 
+     static func isAvailable() -> Bool {
+         return true
+     }
+
  ```
  */
 public protocol AuthProvider {
     func login(withConnection connection: String, scope: String, parameters: [String: Any]) -> NativeAuthTransaction
+
+    /**
+     Determine if the Auth method used by the Provider is available on the device.
+     e.g. If using a Twitter Auth provider it should check the presence of a Twitter account on the device.
+
+     If a Auth is performed on a provider that returns `false` the transaction will fail with an error.
+
+     - returns: Bool if the AuthProvider is available on the device
+     */
+    static func isAvailable() -> Bool
 }
