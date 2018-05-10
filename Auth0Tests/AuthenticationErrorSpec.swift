725758d724f28be40bcf7db7912a44dc3a09aa3a
Martin Walsh <martin.walsh@gmail.com>
2018-05-10 16:03:54 +0100

    Chore

--- Auth0Tests/AuthenticationErrorSpec.swift
+++ Auth0Tests/AuthenticationErrorSpec.swift
@@ -13,7 +13,7 @@
 // all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,u
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
@@ -214,7 +214,7 @@ class AuthenticationErrorSpec: QuickSpec {
                 expect(error.isMultifactorTokenInvalid) == true
             }
 
-            it("should detect invalid malformed mfa token") {
+            it("should detect malformed mfa token") {
                 let values = [
                     "error": "invalid_grant",
                     "error_description": "Malformed mfa_token"
