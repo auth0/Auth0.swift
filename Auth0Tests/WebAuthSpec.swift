f18ab8e429e3cdec18611374594e369b66b5c6f1
andrewfoghel <andrewfoghel@gmail.com>
2019-09-01 17:17:00 -0400

    fix descriptions

--- Auth0Tests/WebAuthSpec.swift
+++ Auth0Tests/WebAuthSpec.swift
@@ -329,14 +329,14 @@ class WebAuthSpec: QuickSpec {
                 expect(controller.modalPresentationStyle) == .fullScreen
             }
             
-            it("should present user overriden presentation style") {
+            it("should present user overridden presentation style") {
                 let auth = newWebAuth().useLegacyAuthentication(withStyle: .overFullScreen)
                 let controller = auth.newSafari(DomainURL, callback: { _ in }).0
                 expect(controller.modalPresentationStyle) == .overFullScreen
             }
             
             if #available(iOS 11.0, *) {
-                it("should present user overriden dismiss button style") {
+                it("should present user overridden dismiss button style") {
                     let auth = newWebAuth()
                         .useLegacyAuthentication(withStyle: .overFullScreen)
                         .setSafariDismissStyle(.cancel)
