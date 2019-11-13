5e6a256df2c4e8dd553064cb69bd53371a8864b6
Drew McLean <online+github@asmclean.net>
2019-11-13 08:20:36 -0500

    Add support for extended profile using SIWA token exchange (#322)
    * Add support for extended profile using SIWA token exchange
    
    * Fix indenting/whitespace
    
    * Revert accidental Cartfile.resolved changes
    
    * Reorder SIWA parameters, and stop sending missing values
    
    * Expand test cases for SIWA

--- Auth0Tests/AuthenticationSpec.swift
+++ Auth0Tests/AuthenticationSpec.swift
@@ -242,6 +242,28 @@ class AuthenticationSpec: QuickSpec {
                 "scope": "openid email",
                 "audience": "https://myapi.com/api"
                 ])) { _ in return authResponse(accessToken: AccessToken, idToken: IdToken) }.name = "Token Exchange Apple Success with custom scope and audience"
+                
+                stub(condition: isToken(Domain) && hasAtLeast([
+                "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",
+                "subject_token": "VALIDNAMECODE",
+                "subject_token_type": "http://auth0.com/oauth/token-type/apple-authz-code"]) &&
+                (hasAtLeast(["user_profile": "{\"name\":{\"lastName\":\"Smith\",\"firstName\":\"John\"}}" ]) || hasAtLeast(["user_profile": "{\"name\":{\"firstName\":\"John\",\"lastName\":\"Smith\"}}" ]))
+                ) { _ in return authResponse(accessToken: AccessToken, idToken: IdToken) }.name = "Token Exchange Apple Success with user profile"
+                
+                stub(condition: isToken(Domain) && hasAtLeast([
+                "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",
+                "subject_token": "VALIDPARTIALNAMECODE",
+                "subject_token_type": "http://auth0.com/oauth/token-type/apple-authz-code",
+                "user_profile": "{\"name\":{\"firstName\":\"John\"}}"
+                ])) { _ in return authResponse(accessToken: AccessToken, idToken: IdToken) }.name = "Token Exchange Apple Success with partial user profile"
+                
+                stub(condition: isToken(Domain) && hasAtLeast([
+                "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",
+                "subject_token": "VALIDMISSINGNAMECODE",
+                "subject_token_type": "http://auth0.com/oauth/token-type/apple-authz-code"]) &&
+                hasNoneOf(["user_profile"])
+                ) { _ in return authResponse(accessToken: AccessToken, idToken: IdToken) }.name = "Token Exchange Apple Success with missing user profile"
+                
             }
 
             it("should exchange apple auth code for credentials") {
@@ -284,8 +306,56 @@ class AuthenticationSpec: QuickSpec {
                 }
             }
             
-        }
+            it("should exchange apple auth code for credentials with fullName") {
+                var fullName = PersonNameComponents()
+                fullName.givenName = "John"
+                fullName.familyName = "Smith"
+                fullName.middleName = "Ignored"
 
+                waitUntil(timeout: Timeout) { done in
+                    auth.tokenExchange(withAppleAuthorizationCode: "VALIDNAMECODE",
+                                       fullName: fullName)
+                        .start { result in
+                            expect(result).to(haveCredentials())
+                            done()
+                    }
+                }
+            }
+            
+            it("should exchange apple auth code for credentials with partial fullName") {
+                var fullName = PersonNameComponents()
+                fullName.givenName = "John"
+                fullName.familyName = nil
+                fullName.middleName = "Ignored"
+                
+                waitUntil(timeout: Timeout) { done in
+                    auth.tokenExchange(withAppleAuthorizationCode: "VALIDPARTIALNAMECODE",
+                                       fullName: fullName)
+                        .start { result in
+                            expect(result).to(haveCredentials())
+                            done()
+                    }
+                }
+            }
+            
+            it("should exchange apple auth code for credentials with missing fullName") {
+                var fullName = PersonNameComponents()
+                fullName.givenName = nil
+                fullName.familyName = nil
+                fullName.middleName = nil
+                
+                waitUntil(timeout: Timeout) { done in
+                    auth.tokenExchange(withAppleAuthorizationCode: "VALIDMISSINGNAMECODE",
+                                       fullName: fullName)
+                        .start { result in
+                            expect(result).to(haveCredentials())
+                            done()
+                    }
+                }
+            }
+            
+        }
+        
         describe("revoke refresh token") {
 
             let refreshToken = UUID().uuidString.replacingOccurrences(of: "-", with: "")
