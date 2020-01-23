96c2617f492a5a9b6b21d3b5e21f74094f3d5e20
Neil Palethorpe <neil.palethorpe@twin3.co.uk>
2020-01-23 16:20:31 +0000

    Exposed additional swift calls to objective c
    Included:
    + loginWithUsernameOrEmail (using realm and audience call)
    + resumeAuthWithUrl
    + renewWithRefreshToken

--- Auth0/_ObjectiveAuthenticationAPI.swift
+++ Auth0/_ObjectiveAuthenticationAPI.swift
@@ -47,6 +47,13 @@ public class _ObjectiveAuthenticationAPI: NSObject {
             .login(usernameOrEmail: username, password: password, connection: connection, scope: scope, parameters: parameters ?? [:])
             .start(handleResult(callback: callback))
     }
+    
+    @objc(loginWithUsernameOrEmail:password:realm:audience:scope:parameters:callback:)
+    public func login(withUsernameOrEmail username: String, password: String, realm: String, audience: String, scope: String, parameters: [String: Any]?, callback: @escaping (NSError?, Credentials?) -> Void) {
+        self.authentication
+            .login(usernameOrEmail: username, password: password, realm: realm, audience: audience, scope: scope, parameters: parameters ?? [:])
+            .start(handleResult(callback: callback))
+    }
 
     @objc(createUserWithEmail:username:password:connection:userMetadata:callback:)
     // swiftlint:disable:next function_parameter_count
@@ -68,6 +75,18 @@ public class _ObjectiveAuthenticationAPI: NSObject {
                     callback(cause as NSError, nil)
                 }
             }
+    }
+    
+    @objc(resumeAuthWithURL:options:)
+    public static func resume(_ url: URL, options: [A0URLOptionsKey: Any]) -> Bool {
+        return resumeAuth(url, options: options);
+    }
+
+    @objc(renewWithRefreshToken:scope:callback:)
+    public func renew(WithRefreshToken refreshToken: String, scope: String, callback: @escaping (NSError?, Credentials?) -> Void) {
+        self.authentication
+            .renew(withRefreshToken: refreshToken, scope: scope)
+            .start(handleResult(callback: callback))
     }
 
     @objc(resetPasswordWithEmail:connection:callback:)
