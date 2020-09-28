448db4c3315d5db9b3957415eb801626b2b4e8d8
Victor Hugo Barros <heyzooi@users.noreply.github.com>
2020-09-28 12:28:27 -0700

    Bugfix for minTTL on CredentialsManager.swift (#420)
    * Update CredentialsManager.swift
    
    * fixing unit tests
    
    * minTTL not being passed along

--- Auth0/CredentialsManager.swift
+++ Auth0/CredentialsManager.swift
@@ -147,7 +147,7 @@ public struct CredentialsManager {
     /// - Note: [Auth0 Refresh Tokens Docs](https://auth0.com/docs/tokens/concepts/refresh-tokens)
     #if WEB_AUTH_PLATFORM
     public func credentials(withScope scope: String? = nil, minTTL: Int = 0, callback: @escaping (CredentialsManagerError?, Credentials?) -> Void) {
-        guard self.hasValid() else { return callback(.noCredentials, nil) }
+        guard self.hasValid(minTTL: minTTL) else { return callback(.noCredentials, nil) }
         if #available(iOS 9.0, macOS 10.15, *), let bioAuth = self.bioAuth {
             guard bioAuth.available else { return callback(.touchFailed(LAError(LAError.touchIDNotAvailable)), nil) }
             bioAuth.validateBiometric {
@@ -162,7 +162,7 @@ public struct CredentialsManager {
     }
     #else
     public func credentials(withScope scope: String? = nil, minTTL: Int = 0, callback: @escaping (CredentialsManagerError?, Credentials?) -> Void) {
-        guard self.hasValid() else { return callback(.noCredentials, nil) }
+        guard self.hasValid(minTTL: minTTL) else { return callback(.noCredentials, nil) }
         self.retrieveCredentials(withScope: scope, minTTL: minTTL, callback: callback)
     }
     #endif
@@ -186,7 +186,7 @@ public struct CredentialsManager {
                                                  expiresIn: credentials.expiresIn,
                                                  scope: credentials.scope)
                 if self.willExpire(newCredentials, within: minTTL) {
-                    let accessTokenLifetime = Int(expiresIn.timeIntervalSinceNow / 1000)
+                    let accessTokenLifetime = Int(expiresIn.timeIntervalSinceNow)
                     // TODO: On the next major add a new case to CredentialsManagerError
                     let error = NSError(domain: "The lifetime of the renewed Access Token (\(accessTokenLifetime)s) is less than minTTL requested (\(minTTL)s). Increase the 'Token Expiration' setting of your Auth0 API in the dashboard or request a lower minTTL",
                         code: -99999,
@@ -204,7 +204,7 @@ public struct CredentialsManager {
 
     func willExpire(_ credentials: Credentials, within ttl: Int) -> Bool {
         if let expiresIn = credentials.expiresIn {
-            return expiresIn < Date(timeIntervalSinceNow: TimeInterval(ttl * 1000))
+            return expiresIn < Date(timeIntervalSinceNow: TimeInterval(ttl))
         }
 
         return false
