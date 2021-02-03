b0fb3b2ee5e79010d1d5e796b714c3c7c4e1de58
Eric Jensen <ericj@kiva.org>
2021-02-02 16:51:54 -0800

    Add tests for multifactorChallenge

--- Auth0Tests/AuthenticationSpec.swift
+++ Auth0Tests/AuthenticationSpec.swift
@@ -255,6 +255,31 @@ class AuthenticationSpec: QuickSpec {
             }
         }
 
+        // MARK:- MFA Challenge
+
+        describe("MFA challenge") {
+
+            beforeEach {
+                stub(condition: isMultifactorChallenge(Domain) && hasAtLeast([
+                    "mfa_token": MFAToken,
+                    "client_id": ClientId,
+                    "challenge_type": "oob otp",
+                    "oob_channel": OOBChannel,
+                    "authenticator_id": AuthenticatorId
+                    ])) { _ in return multifactorChallengeResponse(challengeType: "oob") }.name = "MFA Challenge"
+            }
+
+            it("should request without filters") {
+                waitUntil(timeout: Timeout) { done in
+                    auth.multifactorChallenge(mfaToken: MFAToken, types: ChallengeTypes, channel: OOBChannel, authenticatorId: AuthenticatorId).start { result in
+                        expect(result).to(beSuccessful())
+                        done()
+                    }
+                }
+            }
+
+        }
+
         // MARK:- Refresh Tokens
 
         describe("renew auth with refresh token") {
