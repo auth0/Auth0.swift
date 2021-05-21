69cb7d0fcdde16213ed817291abdff5ddac39852
Mikhail Kouznetsov <mikhail.kouznetsov@gmail.com>
2021-05-21 16:57:59 -0700

    Force unwrap

--- Auth0/Auth0.swift
+++ Auth0/Auth0.swift
@@ -165,16 +165,18 @@ func plistValues(bundle: Bundle) -> (clientId: String, domain: String)? {
     case 0:
         print("Using Auth0 Production")
         path = bundle.path(forResource: "Auth0Production", ofType: "plist")!
-        values = NSDictionary(contentsOfFile: path) as! [String: Any]
+        values = NSDictionary(contentsOfFile: path!) as! [String: Any]
     case 1:
         print("Using Auth0 Beta")
         path = bundle.path(forResource: "Auth0Beta", ofType: "plist")!
-        values = NSDictionary(contentsOfFile: path) as! [String: Any]
+        values = NSDictionary(contentsOfFile: path!) as! [String: Any]
     case 2:
         print("Using Auth0 Development")
         path = bundle.path(forResource: "Auth0Development", ofType: "plist")!
-        values = NSDictionary(contentsOfFile: path) as! [String: Any]
-    default:break
+        values = NSDictionary(contentsOfFile: path!) as! [String: Any]
+    default:
+        print("Inknown environment")
+        break
     }
     
 //     if UserDefaults.standard.integer(forKey: "environment") == 1 {
