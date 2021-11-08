b9051f928d81deeee836a393db10c551edba4f36
Rita Zerrizuela <zeta@widcket.com>
2021-11-08 19:28:23 -0300

    Drop support for Objective-C

--- Auth0/DesktopWebAuth.swift
+++ Auth0/DesktopWebAuth.swift
@@ -14,22 +14,6 @@ public func resumeAuth(_ urls: [URL]) {
     _ = TransactionStore.shared.resume(url)
 }
 
-public extension _ObjectiveOAuth2 {
-
-    /**
-     Resumes the current Auth session (if any).
-
-     - parameter urls: urls received by the macOS application in AppDelegate
-     - warning: deprecated as the SDK will not support macOS versions older than Catalina
-     */
-    @available(*, deprecated, message: "the SDK will not support macOS versions older than Catalina")
-    @objc(resumeAuthWithURLs:)
-    static func resume(_ urls: [URL]) {
-        resumeAuth(urls)
-    }
-
-}
-
 extension ASTransaction: ASWebAuthenticationPresentationContextProviding {
 
     func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
