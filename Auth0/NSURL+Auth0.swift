00ad33926a87e015b2b00246acbe08e7907f5ebd
Rita Zerrizuela <zeta@widcket.com>
2021-11-30 10:48:06 -0300

    Update NSURL+Auth0.swift

--- Auth0/NSURL+Auth0.swift
+++ Auth0/NSURL+Auth0.swift
@@ -1,17 +1,12 @@
 import Foundation
 
 extension URL {
-    /**
-     Returns an Auth0 domain URL given a domain
 
-     - parameter domain: name of your Auth0 account
-
-     - returns: URL of your Auth0 account
-     */
     static func httpsURL(from domain: String) -> URL {
         let prefix = !domain.hasPrefix("https") ? "https://" : ""
         let suffix = !domain.hasSuffix("/") ? "/" : ""
         let urlString = "\(prefix)\(domain)\(suffix)"
         return URL(string: urlString)!
     }
+
 }
