6c4dbeb0122edb4e8355aa121bba5fc8dc7f63e1
Rita Zerrizuela <zeta@widcket.com>
2021-12-01 11:04:40 -0300

    Add tests for global functions (#559)

--- Auth0Tests/Auth0Spec.swift
+++ Auth0Tests/Auth0Spec.swift
@@ -6,9 +6,112 @@ import OHHTTPStubs
 
 private let ClientId = "CLIENT_ID"
 private let Domain = "samples.auth0.com"
+private let Token = "TOKEN"
 
 class Auth0Spec: QuickSpec {
+
     override func spec() {
+
+        describe("global functions") {
+
+            context("authentication") {
+
+                it("should return authentication client with client id & domain") {
+                    let authentication = Auth0.authentication(clientId: ClientId,
+                                                              domain: Domain) as! Auth0Authentication
+                    expect(authentication.clientId) == ClientId
+                    expect(authentication.url.absoluteString) == "https://\(Domain)/"
+                }
+
+                it("should return authentication client with client id, domain & session") {
+                    let session = URLSession(configuration: URLSession.shared.configuration)
+                    let authentication = Auth0.authentication(clientId: ClientId,
+                                                              domain: Domain,
+                                                              session: session) as! Auth0Authentication
+                    expect(authentication.session).to(be(session))
+                }
+
+            }
+
+            context("users") {
+
+                it("should return users client with token & domain") {
+                    let users = Auth0.users(token: Token, domain: Domain) as! Management
+                    expect(users.token) == Token
+                    expect(users.url.absoluteString) == "https://\(Domain)/"
+                }
+
+                it("should return users client with token, domain & session") {
+                    let session = URLSession(configuration: URLSession.shared.configuration)
+                    let users = Auth0.users(token: Token,
+                                            domain: Domain,
+                                            session: session) as! Management
+                    expect(users.session).to(be(session))
+                }
+
+            }
+
+            #if WEB_AUTH_PLATFORM
+            context("web auth") {
+
+                it("should return web auth client with client id & domain") {
+                    let webAuth = Auth0.webAuth(clientId: ClientId,
+                                                domain: Domain) as! Auth0WebAuth
+                    expect(webAuth.clientId) == ClientId
+                    expect(webAuth.url.absoluteString) == "https://\(Domain)/"
+                }
+
+                it("should return web auth client with client id, domain & session") {
+                    let session = URLSession(configuration: URLSession.shared.configuration)
+                    let webAuth = Auth0.webAuth(clientId: ClientId,
+                                                domain: Domain,
+                                                session: session) as! Auth0WebAuth
+                    expect(webAuth.session).to(be(session))
+                }
+
+            }
+            #endif
+
+            #if !SWIFT_PACKAGE
+            describe("plist loading") {
+
+                let bundle = Bundle(for: Auth0Spec.self)
+
+                it("should return authentication client with bundle") {
+                    let auth = Auth0.authentication(bundle: bundle)
+                    expect(auth.url.absoluteString) == "https://\(Domain)/"
+                    expect(auth.clientId) == ClientId
+                }
+
+                it("should return authentication client with bundle & session") {
+                    let session = URLSession(configuration: URLSession.shared.configuration)
+                    let authentication = Auth0.authentication(session: session, bundle: bundle) as! Auth0Authentication
+                    expect(authentication.session).to(be(session))
+                }
+
+                it("should return users client with bundle") {
+                    let users = Auth0.users(token: Token, bundle: bundle)
+                    expect(users.url.absoluteString) == "https://\(Domain)/"
+                }
+
+                #if WEB_AUTH_PLATFORM
+                it("should return web auth client with bundle") {
+                    let auth = Auth0.webAuth(bundle: bundle)
+                    expect(auth.url.absoluteString) == "https://\(Domain)/"
+                    expect(auth.clientId) == ClientId
+                }
+
+                it("should return web auth client with bundle & session") {
+                    let session = URLSession(configuration: URLSession.shared.configuration)
+                    let webAuth = Auth0.webAuth(session: session, bundle: bundle) as! Auth0WebAuth
+                    expect(webAuth.session).to(be(session))
+                }
+                #endif
+
+            }
+            #endif
+
+        }
 
         describe("logging") {
 
@@ -118,26 +221,8 @@ class Auth0Spec: QuickSpec {
 
         }
 
-        #if !SWIFT_PACKAGE
-        describe("plist loading") {
-
-            let bundle = Bundle(for: Auth0Spec.self)
-
-            it("should return authentication endpoint with account from plist") {
-                let auth = Auth0.authentication(bundle: bundle)
-                expect(auth.url.absoluteString) == "https://samples.auth0.com/"
-                expect(auth.clientId) == "CLIENT_ID"
-            }
-
-            it("should return users endpoint with domain from plist") {
-                let users = Auth0.users(token: "TOKEN", bundle: bundle)
-                expect(users.url.absoluteString) == "https://samples.auth0.com/"
-            }
-
-        }
-        #endif
-
     }
+
 }
 
 class MockLogger: Logger {
