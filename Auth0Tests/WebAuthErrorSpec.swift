033323083a1db50095eef99795df9ed935ddb0c2
Rita Zerrizuela <zeta@widcket.com>
2021-12-17 15:37:16 -0300

    Use more descriptive error messages

--- Auth0Tests/WebAuthErrorSpec.swift
+++ Auth0Tests/WebAuthErrorSpec.swift
@@ -80,52 +80,52 @@ class WebAuthErrorSpec: QuickSpec {
         describe("error message") {
 
             it("should return message for no bundle identifier") {
-                let message = "Unable to retrieve the bundle identifier."
+                let message = "Unable to retrieve the bundle identifier from Bundle.main.bundleIdentifier, or it could not be used to build a valid URL."
                 let error = WebAuthError(code: .noBundleIdentifier)
                 expect(error.localizedDescription) == message
             }
 
-            it("should return message for malformed invitation URL") {
+            it("should return message for invalid invitation URL") {
                 let url = "https://samples.auth0.com"
-                let message = "The invitation URL (\(url)) is missing the required query parameters 'invitation' and 'organization'."
-                let error = WebAuthError(code: .malformedInvitationURL(url))
+                let message = "The invitation URL (\(url)) is missing the 'invitation' and/or the 'organization' query parameters."
+                let error = WebAuthError(code: .invalidInvitationURL(url))
                 expect(error.localizedDescription) == message
             }
 
             it("should return message for user cancelled") {
-                let message = "User cancelled Web Authentication."
+                let message = "The user cancelled the Web Auth operation."
                 let error = WebAuthError(code: .userCancelled)
+                expect(error.localizedDescription) == message
+            }
+
+            it("should return message for PKCE not allowed") {
+                let message = "Unable to complete authentication with PKCE. The correct method for Token Endpoint Authentication Method is not set (it should be 'None'). PKCE support needs to be enabled in the settings page of the Auth0 application, by setting the 'ApplicationType' to 'Native' and the 'Token Endpoint Authentication Method' to 'None'."
+                let error = WebAuthError(code: .pkceNotAllowed)
                 expect(error.localizedDescription) == message
             }
 
             it("should return message for no authorization code") {
                 let values: [String: String] = ["foo": "bar"]
-                let message = "No authorization code found in \(values)."
+                let message = "The callback URL is missing the authorization code in its query parameters (\(values))."
                 let error = WebAuthError(code: .noAuthorizationCode(values))
                 expect(error.localizedDescription) == message
             }
 
-            it("should return message for PKCE not allowed") {
-                let message = "Unable to complete authentication with PKCE. PKCE support can be enabled by setting Application Type to 'Native' and Token Endpoint Authentication Method to 'None' for this app in the Auth0 Dashboard."
-                let error = WebAuthError(code: .pkceNotAllowed)
+            it("should return message for id token validation failed") {
+                let message = "The ID Token validation performed after Web Auth login failed. The underlying error can be accessed via the 'cause' property."
+                let error = WebAuthError(code: .idTokenValidationFailed)
+                expect(error.localizedDescription) == message
+            }
+
+            it("should return message for other") {
+                let message = "An error occurred. The 'Error' value can be accessed via the 'cause' property."
+                let error = WebAuthError(code: .other)
                 expect(error.localizedDescription) == message
             }
 
             it("should return message for unknown") {
                 let message = "foo"
                 let error = WebAuthError(code: .unknown(message))
-                expect(error.localizedDescription) == message
-            }
-
-            it("should return the default message for id token validation failed") {
-                let message = "Failed to perform Web Auth operation."
-                let error = WebAuthError(code: .idTokenValidationFailed)
-                expect(error.localizedDescription) == message
-            }
-
-            it("should return the default message for other") {
-                let message = "Failed to perform Web Auth operation."
-                let error = WebAuthError(code: .other)
                 expect(error.localizedDescription) == message
             }
 
