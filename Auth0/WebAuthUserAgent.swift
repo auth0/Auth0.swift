08ad5646f7167a4cb6fa60d46f0c213fdb71aa2d
Rita Zerrizuela <zeta@widcket.com>
2022-05-26 15:55:15 -0300

    Update Auth0/WebAuthUserAgent.swift

--- Auth0/WebAuthUserAgent.swift
+++ Auth0/WebAuthUserAgent.swift
@@ -8,7 +8,7 @@ public protocol WebAuthUserAgent {
     func start()
 
     /// Tears down the external user agent after the web-based operation completed, if needed.
-    /// Auth0.swift will call this method after the callback was received and processed, or after the user cancelled the operation, or after any other error occurred.
+    /// Auth0.swift will call this method after the callback URL was received and processed, or after the user cancelled the operation, or after any other error occurred.
     ///
     /// - Parameter result: The outcome of the web-based operation, containing either an empty success case or an error.
     func finish(with result: WebAuthResult<Void>)
