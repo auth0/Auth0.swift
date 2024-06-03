b9ee6b756396d507a43be810c3ff9f2a55cac8a6
Sai Venkat Desu <saivenkatdesu@gmail.com>
2024-06-03 08:53:21 +0530

    chore: exposed .transactionActiveAlready error case as a public static object to be consumed in switch cases

--- Auth0/WebAuthError.swift
+++ Auth0/WebAuthError.swift
@@ -39,6 +39,10 @@ public struct WebAuthError: Auth0Error {
     /// build a valid URL.
     /// This error does not include a ``Auth0Error/cause-9wuyi``.
     public static let noBundleIdentifier: WebAuthError = .init(code: .noBundleIdentifier)
+    
+    /// There is already an active transaction at the moment; therefore, this newly initiated transaction is canceled.
+    /// This error does not include a ``Auth0Error/cause-9wuyi``.
+    public static let transactionActiveAlready: WebAuthError = .init(code: .transactionActiveAlready)
 
     /// The invitation URL is missing the `invitation` and/or the `organization` query parameters.
     /// This error does not include a ``Auth0Error/cause-9wuyi``.
