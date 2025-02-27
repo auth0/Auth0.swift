778ad022c7aa028ea12b9c26c074bd579f6a8c64
Rita Zerrizuela <zeta@widcket.com>
2025-02-27 21:05:05 +0000

    Remove empty file

--- Auth0/APICredentials.swift
+++ Auth0/APICredentials.swift
@@ -68,6 +68,7 @@ extension APICredentials: Codable {
     /// `Encodable` initializer.
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
+
         try container.encode(accessToken, forKey: .accessToken)
         try container.encode(tokenType, forKey: .tokenType)
         try container.encode(expiresIn.timeIntervalSinceNow, forKey: .expiresIn)
