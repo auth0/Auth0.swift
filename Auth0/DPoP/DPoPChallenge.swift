cd5e242c130c213be019fc30f9a70bc8733f0900
Rita Zerrizuela <zeta@widcket.com>
2025-07-15 12:11:34 +0100

    Add unit tests for DPoPChallenge

--- Auth0/DPoP/DPoPChallenge.swift
+++ Auth0/DPoP/DPoPChallenge.swift
@@ -12,24 +12,38 @@ struct DPoPChallenge {
 
     init?(from response: HTTPURLResponse) {
         guard let header = response.value(forHTTPHeaderField: "WWW-Authenticate"),
-              header.range(of: "DPoP ", options: .caseInsensitive) != nil else { return nil }
-
-        let valuePattern = #"([\x20-\x21\x23-\x5B\x5D-\x7E]+)"#
-        let errorCodePattern = #"error\s?=\s?""# + valuePattern + "\""
-        let errorDescriptionPattern = #"error_description\s?=\s?""# + valuePattern + "\""
-        let compareOptions: String.CompareOptions = [.caseInsensitive, .regularExpression]
-
-        guard let errorCodeRange = header.range(of: errorCodePattern, options: compareOptions) else {
+              header.range(of: "DPoP ", options: .caseInsensitive) != nil else {
             return nil
         }
 
-        let errorCode = String(header[errorCodeRange])
+        let captureGroupName = "value"
+        let captureGroupPattern = #"(?<"# + captureGroupName + #">[\x20-\x21\x23-\x5B\x5D-\x7E]+)"#
+        let errorCodePattern = #"error\s?=\s?""# + captureGroupPattern + "\""
+        let errorDescriptionPattern = #"error_description\s?=\s?""# + captureGroupPattern + "\""
 
-        if let errorDescriptionRange = header.range(of: errorDescriptionPattern, options: compareOptions) {
-            self.init(errorCode: errorCode, errorDescription: String(header[errorDescriptionRange]))
+        guard let errorCode = header.captureGroup(named: captureGroupName, pattern: errorCodePattern) else {
+            return nil
         }
 
-        self.init(errorCode: errorCode, errorDescription: nil)
+        if let errorDescription = header.captureGroup(named: captureGroupName, pattern: errorDescriptionPattern) {
+            self.init(errorCode: errorCode, errorDescription: errorDescription)
+        } else {
+            self.init(errorCode: errorCode, errorDescription: nil)
+        }
+    }
+
+}
+
+extension String {
+
+    func captureGroup(named name: String, pattern: String, options: NSRegularExpression.Options = []) -> String? {
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: options),
+              let firstMatch = regex.firstMatch(in: self, range: NSRange(self.startIndex ..< self.endIndex, in: self)),
+              let captureGroupRange = Range(firstMatch.range(withName: name), in: self) else {
+            return nil
+        }
+
+        return String(self[captureGroupRange])
     }
 
 }
--- Auth0Tests/DPoP/DPoPChallengeSpec.swift
+++ Auth0Tests/DPoP/DPoPChallengeSpec.swift
@@ -1,1 +1,131 @@
+import Foundation
+import Quick
+import Nimble
 
+@testable import Auth0
+
+class DPoPChallengeSpec: QuickSpec {
+    override class func spec() {
+
+        describe("initialization") {
+
+            it("should initialize with error code") {
+                let errorCode = "invalid_request"
+                let challenge = DPoPChallenge(errorCode: errorCode, errorDescription: nil)
+                expect(challenge.errorCode) == errorCode
+                expect(challenge.errorDescription).to(beNil())
+            }
+
+            it("should initialize with error code and description") {
+                let errorCode = "invalid_request"
+                let errorDescription = "Invalid request"
+                let challenge = DPoPChallenge(errorCode: errorCode, errorDescription: errorDescription)
+                expect(challenge.errorCode) == errorCode
+                expect(challenge.errorDescription) == errorDescription
+            }
+
+        }
+
+        describe("parsing") {
+
+            it("should parse a DPoP challenge with an error code") {
+                let errorCode = "invalid_request"
+                let header = "DPoP error=\"\(errorCode)\""
+                let response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                               statusCode: 401,
+                                               httpVersion: nil,
+                                               headerFields: ["WWW-Authenticate": header])!
+                let challenge = DPoPChallenge(from: response)
+
+                expect(challenge).toNot(beNil())
+                expect(challenge?.errorCode) == errorCode
+                expect(challenge?.errorDescription).to(beNil())
+            }
+
+            it("should parse a DPoP challenge with an error code and description") {
+                let errorCode = "invalid_request"
+                let errorDescription = "Invalid request"
+                let header = "DPoP error=\"\(errorCode)\", error_description=\"\(errorDescription)\""
+                let response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                               statusCode: 401,
+                                               httpVersion: nil,
+                                               headerFields: ["WWW-Authenticate": header])!
+                let challenge = DPoPChallenge(from: response)
+
+                expect(challenge).toNot(beNil())
+                expect(challenge?.errorCode) == errorCode
+                expect(challenge?.errorDescription) == errorDescription
+            }
+
+            it("should ignore case when parsing the authentication scheme") {
+                let errorCode = "invalid_request"
+                let header = "dpoP error=\"\(errorCode)\""
+                let response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                               statusCode: 401,
+                                               httpVersion: nil,
+                                               headerFields: ["WWW-Authenticate": header])!
+                let challenge = DPoPChallenge(from: response)
+
+                expect(challenge).toNot(beNil())
+                expect(challenge?.errorCode) == errorCode
+            }
+
+            it("should ignore whitespace when parsing the error code") {
+                let errorCode = "invalid_request"
+                let header = "DPoP error = \"\(errorCode)\""
+                let response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                               statusCode: 401,
+                                               httpVersion: nil,
+                                               headerFields: ["WWW-Authenticate": header])!
+                let challenge = DPoPChallenge(from: response)
+
+                expect(challenge).toNot(beNil())
+                expect(challenge?.errorCode) == errorCode
+            }
+
+            it("should ignore whitespace when parsing the error description") {
+                let errorDescription = "Invalid request"
+                let header = "DPoP error=\"invalid_request\", error_description = \"\(errorDescription)\""
+                let response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                               statusCode: 401,
+                                               httpVersion: nil,
+                                               headerFields: ["WWW-Authenticate": header])!
+                let challenge = DPoPChallenge(from: response)
+
+                expect(challenge).toNot(beNil())
+                expect(challenge?.errorDescription) == errorDescription
+            }
+
+            it("should return nil when the DPoP challenge does not contain an error code") {
+                let header = "Bearer realm=\"example\", DPoP"
+                let response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                               statusCode: 401,
+                                               httpVersion: nil,
+                                               headerFields: ["WWW-Authenticate": header])!
+
+                expect(DPoPChallenge(from: response)).to(beNil())
+            }
+
+            it("should return nil when WWW-Authenticate header does not contain a DPoP challenge") {
+                let header = "Bearer realm=\"example\""
+                let response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                               statusCode: 401,
+                                               httpVersion: nil,
+                                               headerFields: ["WWW-Authenticate": header])!
+
+                expect(DPoPChallenge(from: response)).to(beNil())
+            }
+
+            it("should return nil when the WWW-Authenticate header is missing") {
+                let response = HTTPURLResponse(url: URL(string: "https://example.com")!,
+                                               statusCode: 401,
+                                               httpVersion: nil,
+                                               headerFields: nil)!
+
+                expect(DPoPChallenge(from: response)).to(beNil())
+            }
+
+        }
+
+    }
+}
