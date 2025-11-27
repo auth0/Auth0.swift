74555996c54f75090e810d3fba8c1154ad3f9bd2
Nandan Prabhu <nandan.prabhup@okta.com>
2025-11-27 17:03:51 +0530

    Fixes build for TV, visionOS and macOS

--- App/Auth0App.swift
+++ App/Auth0App.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import Auth0
 
 @main
-struct AppAuth0App: App {
+struct Auth0App: App {
     var body: some Scene {
         WindowGroup {
             ContentView(viewModel: ContentViewModel(credentialsManager: CredentialsManager(authentication: Auth0.authentication())))
